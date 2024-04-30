5bb623f8b04ae39a962f71eb46b1b531495eb62a
choong <ksky9230@naver.com>
2024-04-30 14:22:29 +0900

    first commit

---
+++ src/test/java/com/ch/anyway/AnywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.anyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
